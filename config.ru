8895861944d61595b5373449de08a117d9d5e5c8
Diogo Santos <diogo@diogosantos.com>
2010-12-07 22:35:51 -0200

    first commit

---
+++ config.ru
@@ -0,0 +1,7 @@
+# This file is used by Rack-based servers to start the application.
+
+Encoding.default_internal = "UTF-8"
+Encoding.default_external = "UTF-8"
+
+require ::File.expand_path('../config/environment',  __FILE__)
+run Rails3template::Application
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3template::Application.config.secret_token = '690392b1003360ecbe9d2879a4cf1dd4a38c73e78c599da90ea1c7828747708e8be38630fb1e184086c2eab11d7a78c885fb5176b8bdc79c4808e416fd0976e4'
